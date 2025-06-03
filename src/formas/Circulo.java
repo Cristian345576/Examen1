fb6baed79d8f6e25e7af57371c738461c1e27a8a
Cristian <cristian.barquero@isepceu.es>
2025-06-03 08:14:33 +0200

    circulo

---
+++ src/formas/Circulo.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package formas;
+
+/**
+ * 
+ */
+public class Circulo {
+	private int id;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+	/**
+	 * @return the radio
+	 */
+	public double getRadio() {
+		return radio;
+	}
+	/**
+	 * @param radio the radio to set
+	 */
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	private String color;
+	@Override
+	public String toString() {
+		return "Circulo [id=" + id + ", color=" + color + ", radio=" + radio + ", nombre=" + nombre + "]";
+	}
+	private double radio;
+	private String nombre;
+
+}
